d0c8c482b254759f49e81eda430be5d9526a655e
Mariusz Kwiatek <ototto@protonmail.com>
2017-01-23 06:30:22 +0100

    Init

---
+++ Mino/Mino/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mino.Startup))]
+namespace Mino
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
