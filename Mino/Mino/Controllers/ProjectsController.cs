1ade35d231bba857c6bee380ede91f5d08d6ddca
Mariusz Kwiatek <ototto@protonmail.com>
2017-02-04 12:00:35 +0100

    Add manage projects view

---
+++ Mino/Mino/Controllers/ProjectsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNet.Identity;
+using Mino.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Mino.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public ProjectsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        public ActionResult Show()
+        {
+            var userId = User.Identity.GetUserId();
+            var projects = _context.Projects.Where(x =>
+                x.UserId == userId)
+                .ToList();
+
+            return View(projects);
+        }
+    }
+}
