e7c49cfda518df7a70e44a84ed49b47010c2fc43
MariuszKwiatek <ototto1231@gmail.com>
2017-03-01 07:11:53 +0100

    Fixed login error.

--- Mino/Mino/Controllers/AccountController.cs
+++ Mino/Mino/Controllers/AccountController.cs
@@ -149,7 +149,7 @@ namespace Mino.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Name, Email = model.Email };
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
